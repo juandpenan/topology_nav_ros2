a47f05f178732a6a8750552aabd20afdfad58e54
juan <juan97pena@gmail.com>
2023-10-12 13:54:54 +0200

    initial pose capability added

--- topological_mapping/topological_mapping/topological_mapping/images_to_map.py
+++ topological_mapping/topological_mapping/topological_mapping/images_to_map.py
@@ -14,6 +14,7 @@ from topological_mapping.topological_mapping.topological_map import TopologicalM
 
 from vqa_ros.vqa_ros import VQAModel
 
+
 class SaveMapToDisc(VQAModel):
     def __init__(self):
         super().__init__()
@@ -27,28 +28,21 @@ class SaveMapToDisc(VQAModel):
         self.declare_parameter('question_qty', 10)
         self.declare_parameter('state_qty', 8)
         self.declare_parameter('max_images_per_state', 10)
+        self.declare_parameter('resume_map_path', '')
 
+        self.resume_map_path = self.get_parameter('resume_map_path').get_parameter_value().string_value
         self.state_qty = self.get_parameter('state_qty').get_parameter_value().integer_value
         self.question_qty = self.get_parameter('question_qty').get_parameter_value().integer_value
         self.question_depth = self.get_parameter(
             'max_images_per_state').get_parameter_value().integer_value
 
+        self.timer.destroy()
         self.create_subscription(OccupancyGrid,
                                  '/map',
                                  self.map_callback,
                                  qos_profile=map_qos_profile)
 
-    def map_callback(self, map_msg):
-        self.map_msg = map_msg
-        map = TopologicalMap(map_msg, self.state_qty, self.question_qty)
-
-        rootdir = str(pathlib.Path(__file__).parent.resolve()).removesuffix(
-            '/topological_mapping/topological_mapping') + '/data/'
-
-        rootdir = str(pathlib.Path(__file__).parent.resolve()).removesuffix(
-            '/topological_mapping/topological_mapping') + '/real/data/'
-
-        state = 0
+    def _save_to_disc(self, map, rootdir):
 
         for subdir, dirs, files in os.walk(rootdir):
             try:
@@ -70,40 +64,56 @@ class SaveMapToDisc(VQAModel):
             # for each state folder
             for subdir, dirs, files in os.walk(subdir):
                 self.get_logger().info('saving in index :'+str(index))
-                global_conf = np.zeros(shape=(self.question_depth,), dtype='f')
-                global_answers = np.zeros(shape=(self.question_depth,), dtype='U16')
-                qdpth = 0
-                # for each image in each state folder                 
-                for file in files:
-                    image = cv2.imread(subdir+'/'+file)                                                                    
+
+                for q_qty, question in enumerate(self.questions):
+
                     answers = []
-                    conf = []
-                    q_qty = 0
-                    for question in self.questions:                          
+                    confs = []
+
+                    for qdpth, file in enumerate(files):
+
+                        if question.find('*') != -1:
+                            question = question.replace('*', glob['q_a'][0][q_qty-1][0])
+
+                        image = cv2.imread(subdir+'/'+file)
                         answer, confi = self._plot_inference_qa(image, question)
-                        self.get_logger().info(f'appending in array {q_qty} {qdpth}')
-                        glob['index'][0] = index
-                        glob['q_a'][0][q_qty][qdpth] = answer
-                        glob['acc'][0][q_qty][qdpth] = confi
-                        q_qty += 1
-                        # answers.append(answer)
-                        # conf.append(confi)
-                #     global_conf[i] = conf
-                #     global_answers.append[i] = answers
-                    qdpth += 1
-                # global_conf = np.array(global_conf).T
-                # global_answers = np.array(global_answers).T
+
+                        if not answers and not confs:
+                            answers.append(answer)
+                            confs.append(confi)
+
+                        else:
+
+                            if answer in answers:
+                                confs[answers.index(answer)] = (confs[answers.index(answer)]
+                                                                + confi) / 2.0
+                            else:
+                                answers.append(answer)
+                                confs.append(confi)
+
+                    glob['index'][0] = index
+                    glob['q_a'][0][q_qty][0: len(answers)] = answers
+                    glob['acc'][0][q_qty][0: len(answers)] = confs
 
                 if map.is_map_empty(index=index):
                     map.update_map(glob_features=glob, index=index)
                     self.get_logger().info('map updated cuz it was empty')
             self.get_logger().info('savig map')
             map.save_map(path=str(pathlib.Path(__file__).parent.resolve()).removesuffix(
-                '/topological_mapping/topological_mapping')+'/map/map6',
-                         questions=self.questions, name='map6')
+                '/topological_mapping/topological_mapping')+'/map/map13',
+                         questions=self.questions, name='map13')
             self.get_logger().info('map saved')
 
-            state += 1
+        return
+
+    def map_callback(self, map_msg):
+        self.map_msg = map_msg
+        map = TopologicalMap(map_msg, self.state_qty, self.question_qty)
+
+        rootdir = str(pathlib.Path(__file__).parent.resolve()).removesuffix(
+            '/topological_mapping/topological_mapping') + '/data/' + '/experiments/'
+
+        self._save_to_disc(map, rootdir)
 
         self.get_logger().info('Finished')
         self.destroy_node()
@@ -117,6 +127,7 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
